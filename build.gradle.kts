a0bcd4c59756bb7946d910e50a6abc2fff5f666e
ArllanFelipe Santigo Correia <arllansantiago@gmail.com>
2023-08-19 22:10:45 -0300

    Projeto Inicializado

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.14"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "br.com.dio"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
